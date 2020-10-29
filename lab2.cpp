d1962f7aaf88d7ba23ca04dead611c5e5d78c811
Aldo Bujevic <aldobujevic97@gmail.com>
2020-10-29 11:54:30 +0100

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -13,6 +13,9 @@ void AddBegin(Pozicija P, char* n, char *s, int y); //pass Head
 void AddEnd(Pozicija P, char* n, char *s, int y); //pass Head
 Pozicija FindSurname(Pozicija P, char *s); //pass Head->next
 void DeletePerson(Pozicija P, char *s); //pass Head
+void DeletePerson(Pozicija P, Pozicija A); //pass Head
+void AddAfter(Pozicija P, Pozicija A); //pass head
+void AddBefore(Pozicija P, Pozicija A); //pass head->next
 
 int main(){
 
@@ -20,17 +23,19 @@ Pozicija Head = (Pozicija)malloc(sizeof(person));
 Head->next = NULL;
 
 Print(Head->next);
-AddBegin(Head,"aldo", "bujevic", 1997);
+AddBegin(Head,(char*)"aldo",(char*) "bujevic", 1997);
 Print(Head->next);
-AddBegin(Head,"mimi", "lija", 2000);
-AddBegin(Head,"lala", "teletabi", 2004);
-AddBegin(Head,"roko", "gradinski", 2012);
+AddBegin(Head,(char*)"mimi",(char*) "lija", 2000);
+AddBegin(Head,(char*)"lala", (char*)"teletabi", 2004);
+AddBegin(Head,(char*)"roko", (char*)"lija", 2012);
 
-AddEnd(Head,"jure", "zvoncic", 2015);
+AddEnd(Head,(char*)"jure", (char*)"zvoncic", 2015);
 Print(Head->next);
-Pozicija P = FindSurname(Head->next,"lija");
+Pozicija P = FindSurname(Head->next,(char*)"lija");
 if(P!=NULL) printf("---%s\t%s\t%d---\n",P->name,P->surname,P->year);
-DeletePerson(Head,"lija");
+
+DeletePerson(Head,(char*)"lija");
+
 Print(Head->next);
 
 return 0;
@@ -62,26 +67,51 @@ void AddEnd(Pozicija P, char* n, char *s, int y){
     return;
 }
 Pozicija FindSurname(Pozicija P, char *s){
-    while(P->next != NULL){
-        //printf("Comparing %s with %s\n",P->surname,s);
-        if(P->surname == s){
+    if(P!=NULL){
+        while(P->next != NULL){
             //printf("Comparing %s with %s\n",P->surname,s);
-            return P;
-            }
-        P=P->next;
+            if(P->surname == s){
+                //printf("Comparing %s with %s\n",P->surname,s);
+                return P;
+                }
+            P=P->next;
+        }
     }
     //printf("NO MATCHES\n");
     return NULL;
 }
-void DeletePerson(Pozicija P, char *s){
-    Pozicija temp=P->next;
-    while(temp->next != NULL){
-        if(temp->surname == s){
-            P->next = temp->next;
-            free(temp);
-            }
-        P=P->next;
-        temp=temp->next;
+void DeletePerson(Pozicija P, char* s){
+    Pozicija temp = P;
+    P = P->next;
+    if(P!=NULL){
+        while(P->next != NULL){
+            if(P->surname == s){
+                temp->next = P->next;
+                free(P);
+                P=temp;
+                //break; //ako zelimo da brise samo prvu osobu u listi s tim prezimenom
+                }
+            temp = P;
+            P = P->next;
+        }
+    }
+    return;
+}
+
+void DeletePerson(Pozicija P, Pozicija A){
+    Pozicija temp = P;
+    P = P->next;
+    if(P!=NULL){
+        while(P->next != NULL){
+            if(P == A){
+                temp->next = P->next;
+                free(P);
+                P=temp;
+                //break; //ako zelimo da brise samo prvu osobu u listi s tim prezimenom
+                }
+            temp = P;
+            P = P->next;
+        }
     }
     return;
 }
